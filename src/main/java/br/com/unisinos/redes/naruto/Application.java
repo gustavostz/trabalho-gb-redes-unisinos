acfff1412f3a4d4ddc7209520d941424d08114aa
Gustavo Steinmetz <stzgustavo@gmail.com>
2020-05-21 21:04:52 -0300

    estrutura inicial e socket funcionando

---
+++ src/main/java/br/com/unisinos/redes/naruto/Application.java
@@ -0,0 +1,7 @@
+package br.com.unisinos.redes.naruto;
+
+public class Application {
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/main/java/br/com/unisinos/redes/naruto/domain/Ninja.java
@@ -0,0 +1,10 @@
+package br.com.unisinos.redes.naruto.domain;
+
+public class Ninja {
+
+    private int poder;
+
+    private int vida;
+
+    private int chackra;
+}
